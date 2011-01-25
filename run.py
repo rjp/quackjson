d5394488ecd5811187df2504ca842de2c8406589
Paul Waring <paul@xk7.net>
2011-01-25 17:52:41 +0000

    Simplified run.py script.

--- run.py
+++ run.py
@@ -1,15 +1,8 @@
 from quack import qUAck
 
-client = qUAck(filename = 'qUAckrc')
-#client.get_folders(subscribed_only = True)
-
-#client.post_message(folder = 'Private', subject = 'quackjson Test', body = 'Hello', to = 'pwaring')
-
+client = qUAck(filename = 'qUAckrc.sample')
 messages = client.get_folder_messages('Private')
-#print messages
 
 for message in messages:
   print message
-  #full_msg = client.get_message(message['id'])
   
-  #print full_msg
