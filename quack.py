30b136fbd44f06264e47ec31a323df5f57a441fe
Paul Waring <paul@xk7.net>
2011-01-26 21:07:00 +0000

    qUAck displays the time the user has been logged on in the format HH:mm. Whilst quackjson does not have the concept of a 'login', as HTTP is stateless, the effect can be emulated by keeping track of how long the client has been running. In theory, this information could be sent to the server on a regular basis for the wholist.
    Changes to allow this feature:
    
    1. Log the current time when the client is started (self.start_time).
    
    2. New function get_elapsed_time, which creates a string of the difference between the current time and the start time (this could be abstracted to get_elasped_time and get_elasped_time_str at a later date).
    
    3. New function get_menu_text, which returns a string representing the main menu, including the elapsed time.

--- quack.py
+++ quack.py
@@ -1,6 +1,26 @@
 from uaclient import UAClient
+from datetime import datetime
 
 class qUAck(UAClient):
   def __init__(self, username = None, password = None, filename = None):
     UAClient.__init__(self, username = username, password = password, filename = filename)
     
+    self.start_time = datetime.now()
+  
+  def get_menu_text(self):
+    return '+' + self.get_elapsed_time() + ' Main (BCDE, ?+ for help): '
+    
+  def get_elapsed_time(self):
+    current_time = datetime.now()
+    time_diff = current_time - self.start_time
+    
+    minutes, seconds = divmod(time_diff.seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    
+    # Convert to string and pad with zeroes
+    hours = str(hours).zfill(2)
+    minutes = str(minutes).zfill(2)
+    
+    elapsed_time = hours + ':' + minutes
+    
+    return elapsed_time
