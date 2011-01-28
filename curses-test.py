896b8d1f2d5f4b8f34ec08418c0a105950c1d1c2
Paul Waring <paul@xk7.net>
2011-01-28 22:30:45 +0000

    Working curses test script, with simple menu.

---
+++ curses-test.py
@@ -0,0 +1,25 @@
+import curses
+from quack import qUAck
+
+def duck(stdscr):
+  client = qUAck(username = 'quackjson', password = 'qjson')	
+  stdscr.addstr(client.get_menu_text())
+  stdscr.refresh()
+
+  menu_continue = True
+
+  while menu_continue:
+    c = stdscr.getch()
+
+    if c == ord('l') or c == ord('L'):
+      stdscr.addstr("\nList of folders")
+      stdscr.addstr("\n"+client.get_menu_text())
+    elif c == ord('q') or c == ord('Q'):
+    	menu_continue = False
+    else:
+    	stdscr.addstr("\n"+client.unrecognised_command)
+    	stdscr.addstr("\n"+client.get_menu_text())
+    
+    stdscr.refresh()
+
+curses.wrapper(duck)
