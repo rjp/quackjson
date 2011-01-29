e26a2ca87816d29c6f1ea5835f9f04588821d172
Paul Waring <paul@xk7.net>
2011-01-29 11:34:19 +0000

    Tidying up whitespace.

--- uaclient.py
+++ uaclient.py
@@ -31,64 +31,64 @@ class UAClient:
 
     self.client = httplib2.Http()
     self.client.add_credentials(self.username, self.password)
-  
+
   def send_request(self, path, method = 'GET', params = {}, body = None):
     uri = self.base_uri + path
-    
+
     headers = {}
     headers['Accept'] = 'application/json'
-    
+
     if method == 'GET':
       response, data = self.client.request(uri, 'GET', headers = headers)
     elif method == 'POST':
       headers['Content-Type'] = 'application/json'    
       response, data = self.client.request(uri, 'POST', body = body, headers = headers)
-    
+
     data = json.loads(data)
-  
+
     return response, data
 
   def get_folders(self, subscribed_only = False, unread_only = False):
     # Default is to fetch all folders
     path = '/folders'
-    
+
     if subscribed_only:
       path += '/subscribed'
     else:
       path += '/all'
-    
+
     if unread_only:
       path += '/unread'
-    
+
     response, folders = self.send_request(path)
-    
+
     return folders
-  
+
   def get_folder_message_list(self, folder, unread_only = True):
     path = '/folder/' + folder
-    
+
     if unread_only:
       path += '/unread'
-      
+
     response, messages = self.send_request(path)
-    
+
     return messages
-    
+
   def get_folder_messages(self, folder, unread_only = True):
     message_list = self.get_folder_message_list(folder, unread_only)
-    
+
     messages = []
-    
+
     for message in message_list:
       messages.append(self.get_message(message['id']))
-    
+
     return messages
-    
+
   def get_message(self, id):
     path = '/message/' + str(id)
-    
+
     response, message = self.send_request(path)
-    
+
     return message
 
   def post_message(self, folder, subject, body, to = '',):
@@ -96,27 +96,27 @@ class UAClient:
     message['subject'] = subject
     message['to'] = to
     message['body'] = body
-    
+
     body = json.dumps(message)
-    
+
     path = '/folder/' + folder
-    
+
     response, data = self.send_request(path, method = 'POST', body = body)
-    
+
   def change_subscription(self, folder, change):
     subscription = {}
     subscription['folder'] = folder
-    
+
     body = json.dumps(subscription)
-    
+
     path = '/folder/' + folder + '/' + change
-    
+
     response, data = self.send_request(path, method = 'POST', body = body)
 
     return response, data
 
   def subscribe(self, folder):
     return self.change_subscription(folder, 'subscribe')
-    
+
   def unsubscribe(self, folder):
     return self.change_subscription(folder, 'unsubscribe')
