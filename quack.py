438450ab8671e1102fafe60823c5d1f1f016035d
Paul Waring <paul@xk7.net>
2011-01-29 12:00:36 +0000

    1. Support for changing colour text, initially only yellow/black for menu. 2. Allow users to request all folders - though this currently crashes curses.

--- quack.py
+++ quack.py
@@ -3,22 +3,26 @@ from uaclient import UAClient
 from datetime import datetime
 
 class qUAck(UAClient):
+  yellow_black = 1
+
   def __init__(self, username = None, password = None, filename = None, debug = False):
     UAClient.__init__(self, username = username, password = password, filename = filename, debug = debug)
 
     self.start_time = datetime.now()
     curses.wrapper(self.startcurses)
 
-  def get_menu_text(self, menu, char_options, elapsed_time = False):
-    menu_text = ''
+  def print_menu_text(self, menu, char_options, elapsed_time = False):
+    self.stdscr.addstr("\n")
 
     if elapsed_time:
-      menu_text = '+' + self.get_elapsed_time() + ' '
+      self.stdscr.addstr('+' + self.get_elapsed_time(), curses.color_pair(self.yellow_black))
+      self.stdscr.addstr(' ')
 
-    menu_text += menu
-    menu_text += ' (' + ''.join(char_options).upper() + ', ?+ for help): '
-
-    return menu_text
+    self.stdscr.addstr(menu)
+    self.stdscr.addstr(' (')
+    self.stdscr.addstr(''.join(char_options).upper(), curses.color_pair(self.yellow_black))
+    self.stdscr.addstr(', ?+ for help): ')
+    self.stdscr.refresh()
 
   def get_elapsed_time(self):
     current_time = datetime.now()
@@ -36,6 +40,8 @@ class qUAck(UAClient):
     return elapsed_time
 
   def startcurses(self, stdscr):
+    curses.init_pair(self.yellow_black, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
     self.stdscr = stdscr
     self.main_menu()
 
@@ -58,8 +64,7 @@ class qUAck(UAClient):
     menu_continue = True
 
     while menu_continue:
-      self.stdscr.addstr("\n" + self.get_menu_text('Main', char_options, elapsed_time = True))
-      self.stdscr.refresh()
+      self.print_menu_text('Main', char_options, elapsed_time = True)
 
       c = self.stdscr.getch()
 
@@ -72,18 +77,20 @@ class qUAck(UAClient):
         self.unrecognised_command()
 
   def folder_list_menu(self):
-    char_options = ['s', 'x']
+    char_options = ['a', 's', 'x']
     menu_options = self.get_menu_options(char_options)
     menu_continue = True
 
     while menu_continue:
-      self.stdscr.addstr("\n" + self.get_menu_text('Folders', char_options))
-      self.stdscr.refresh()
+      self.print_menu_text('Folders', char_options)
 
       c = self.stdscr.getch()
 
       if c in menu_options:
         if c == ord('x'):
+          menu_continue = False
+        elif c == ord('a'):
+          self.print_folder_list(self.get_folders())
           menu_continue = False
         elif c == ord('s'):
           self.print_folder_list(self.get_folders(subscribed_only = True))
@@ -94,5 +101,6 @@ class qUAck(UAClient):
   def print_folder_list(self, folders):
     for folder in folders:
       self.stdscr.addstr("\n" + folder['folder'])
+      self.stdscr.refresh()
 
     self.stdscr.refresh()
