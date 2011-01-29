5f2ce17e83b421fac7c3641b26a99b6d8963950b
Paul Waring <paul@xk7.net>
2011-01-29 14:03:01 +0000

    New run script.

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
 from quack import qUAck
 
-client = qUAck(filename = 'qUAckrc.sample')
-messages = client.get_folder_messages('Private')
-
-for message in messages:
-  print message
-  
+client = qUAck(filename = 'quackrc')
